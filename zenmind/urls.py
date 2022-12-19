c7f31edc8f065be49dbb67f982e3161f41aaf2db
Eren Ohtaroglu <eren.ohtaroglu@skf.com>
2022-12-19 18:23:34 +0100

    Dockerized. Pagination Complete. User based sorting of posts done.

--- zenmind/urls.py
+++ zenmind/urls.py
@@ -1,25 +1,24 @@
 from django.urls import path
-from .views import  (
+from .views import (
       PostListView,
       PostDetailView,
       PostCreateView,
       PostUpdateView,
-      PostDeleteView
+      PostDeleteView,
+      UserPostListView
 )
 from . import views
 
-
-
 urlpatterns = [
-
       path('', PostListView.as_view(), name='zenmind-home'),
+      path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
       path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
       path('post/new/', PostCreateView.as_view(), name='post-create'),
       path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
       path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
       path('about/', views.about, name='zenmind-about'),
       
-#path to be used in login.
-#   path('accounts/login/', views.LoginView.as_view(), name='login'),
+# path to be used in login.
+# path('accounts/login/', views.LoginView.as_view(), name='login'),
 
 ]
